774c5606669742ff30e0d44eb71575a691e44a6c
Amneharoun <thalaadam14@gmail.com>
2025-09-26 15:26:55 +0100

    fixe autmiddleware

--- backend-server/middlewares/authMiddelware.js
+++ backend-server/middlewares/authMiddelware.js
@@ -1,29 +1,35 @@
 // middlewares/auth.js
 const jwt = require("jsonwebtoken");
 
-const authMiddleware = (roles = []) => {
-  return (req, res, next) => {
-    try {
-      const token = req.headers.authorization?.split(" ")[1];
-      if (!token) {
-        console.log("Accès refusé, token manquant");
-        
-        return res.status(401).json({ message: "Accès refusé, token manquant" });
-      }
-
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.user = decoded;
-
-      // Si on a donné des rôles autorisés
-      // if (roles.length && !roles.includes(decoded.role)) {
-      //   return res.status(403).json({ message: "Accès interdit" });
-      // }
-
-      next();
-    } catch (err) {
-      res.status(401).json({ message: "Token invalide" });
+const authMiddleware = async (req, res, next) => {
+  try {
+    if (!req.headers.authorization) {
+      return res.status(400).send({
+        message: "Accès refusé, token manquant",
+      });
     }
-  };
+
+    const token = req.headers.authorization.split(" ")[1];
+    if (!token) {
+      console.log("Accès refusé, token manquant");
+
+      return res.status(401).json({ message: "Accès refusé, token manquant" });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = decoded;
+
+    // Si on a donné des rôles autorisés
+    // if (roles.length && !roles.includes(decoded.role)) {
+    //   return res.status(403).json({ message: "Accès interdit" });
+    // }
+
+    next();
+  } catch (error) {
+    console.log(error);
+
+    res.status(401).json({ message: "Token invalide" });
+  }
 };
 
 module.exports = authMiddleware;
