6a24650f9924ee668e744db697a7b9b712f0aa74
Amneharoun <thalaadam14@gmail.com>
2025-10-24 14:29:01 +0100

    fix: autMiddelware

--- backend-server/middlewares/authMiddelware.js
+++ backend-server/middlewares/authMiddelware.js
@@ -4,7 +4,7 @@ const jwt = require("jsonwebtoken");
 const authMiddleware = async (req, res, next) => {
   try {
     if (!req.headers.authorization) {
-      return res.status(400).send({
+      return res.status(401).send({
         message: "Accès refusé, token manquant",
       });
     }
@@ -28,8 +28,31 @@ const authMiddleware = async (req, res, next) => {
   } catch (error) {
     console.log(error);
 
-    res.status(401).json({ message: "Token invalide" });
+    res.status(403).json({ message: "Token invalide" });
   }
 };
 
 module.exports = authMiddleware;
+
+// const jwt = require("jsonwebtoken");
+// const User = require("../models/userModel");
+
+// authMiddleware = async (req, res, next) => {
+//   try {
+//     const authHeader = req.headers.authorization;
+//     if (!authHeader) return res.status(401).json({ message: "Token manquant" });
+
+//     const token = authHeader.split(" ")[1];
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+//     const user = await User.findById(decoded.id);
+//     if (!user) return res.status(403).json({ message: "Utilisateur supprimé ou inexistant" });
+
+//     req.user = user;
+//     next();
+//   } catch (error) {
+//     res.status(403).json({ message: "Accès refusé", error: error.message });
+//   }
+// };
+
+// module.exports = authMiddleware;
