c4efe4b19ed65424351f0de16ccdb9e7b20a8ad4
Amneharoun <thalaadam14@gmail.com>
2025-11-17 13:07:12 +0100

    configs

--- front-end/src/config.js
+++ front-end/src/config.js
@@ -1,4 +1,4 @@
 // src/config.js
-const API_URL = import.meta.env.API_URL || "http://localhost:5000";
-// const API_URL = "https://project-final-lsby.onrender.com";
+// const API_URL = import.meta.env.API_URL || "http://localhost:5000";
+const API_URL = "https://project-final-lsby.onrender.com";
 export default API_URL;
